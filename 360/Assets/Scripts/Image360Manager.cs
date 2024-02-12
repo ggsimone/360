5c9b709d8ef618dd9ef63c85ea89a021e674aa73
Alexandre Raimbault <122281737+araim19@users.noreply.github.com>
2024-02-12 14:30:41 +0100

    Scene with skybox

---
+++ 360/Assets/Scripts/Image360Manager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Image360Manager : MonoBehaviour
+{
+
+    public Material[] Randonnee;
+    private int index = 0;
+
+    void Start()
+    {
+        ChangeSkyboxAuto();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void ChangeSkyboxAuto()
+    {
+        RenderSettings.skybox = Randonnee[index];
+    }
+
+    private void ChangeSkyboxIndex(int idx)
+    {
+        RenderSettings.skybox = Randonnee[idx];
+    }
+}
