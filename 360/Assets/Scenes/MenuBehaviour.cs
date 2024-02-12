403f10ce811f676c3c15b8b376218efd92c8446e
z.fourrier <zacharie.fourrier@gmail.com>
2024-02-12 12:26:19 +0100

    Menu is ready for use

---
+++ 360/Assets/Scenes/MenuBehaviour.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuBehaviourScript : MonoBehaviour
+{
+    public Button _btnQuit;
+    public Button _btnStartGuided;
+    public Button _btnStartFree;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _btnQuit.onClick.AddListener(quitBtn);
+        _btnStartGuided.onClick.AddListener(startGuidedBtn);
+        _btnStartFree.onClick.AddListener(startFreeBtn);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void quitBtn()
+    {
+        Debug.Log("App quit called");
+        Application.Quit();
+    }
+
+    void startGuidedBtn()
+    {
+        Debug.Log("Starting guided playthrough");
+    }
+
+    void startFreeBtn()
+    {
+        Debug.Log("Starting free playthrough");
+    }
+}
